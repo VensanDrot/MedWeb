69f2c780887759f3511b4007169a709fe4f4a3ea
VensanDrot <vensandrot@gmail.com>
2023-07-01 02:46:37 -0400

    small changes css for phone button

--- src/components/FloatingButtons/index.tsx
+++ src/components/FloatingButtons/index.tsx
@@ -4,10 +4,12 @@ import { AiOutlinePhone } from "react-icons/ai";
 
 const FloatingButtons = () => {
   return (
-    <div className={` ${styles.floating_Container} yellow `}>
-      <a href="#">
-        <AiOutlinePhone /> <span>(999) 999 9999</span>
-      </a>
+    <div className={styles.floating_Container}>
+      <div className={` ${styles.floating_Container_mini} yellow `}>
+        <a href="#">
+          <AiOutlinePhone /> <span>(999) 999 9999</span>
+        </a>
+      </div>
     </div>
   );
 };
