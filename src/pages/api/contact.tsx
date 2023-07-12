8feb942d902f920b2b266675f36b1e58d4ad5d13
VensanDrot <vensandrot@gmail.com>
2023-07-12 01:52:16 -0400

    checking error origin

--- src/pages/api/contact.tsx
+++ src/pages/api/contact.tsx
@@ -5,36 +5,48 @@ import path from "path";
 const handlebarOptions: any = {
   viewEngine: {
     extName: ".handlebars",
-    partialsDir: path.resolve("./src/views"),
+    partialsDir: path.resolve("./src/pages/views"),
     defaultLayout: false,
   },
-  viewPath: path.resolve("./src/views"),
+  viewPath: path.resolve("./src/pages/views"),
   extName: ".handlebars",
 };
 
 transporter.use("compile", hbs(handlebarOptions));
 
+//handle sending email
 const handler = async (req: any, res: any) => {
+  //check if method is POST
   if (req.method === "POST") {
     const data = req.body;
-    if (!data || !data.name || !data.email || !data.subject || !data.message) {
-      return res.status(400).send({ message: "Bad request" });
+    if (!data.name || !data.email || !data.number || !data.message || !data.subject) {
+      return res.status(400).json({ error: "Bad request" });
     }
+    const mailOptions = {
+      //...generateEmailContent(data),
+      from: process.env.EMAIL,
+      to: data.email,
+      subject: data.subject,
+      template: "email",
+      context: {
+        name: data.name,
+        subject: data.subject,
+        number: data.number,
+        email: data.email,
+        message: data.message,
+      },
+    };
 
     try {
-      await transporter.sendMail({
-        from: process.env.NEXT_PUBLIC_EMAIL,
-        to: data.email,
-        html: `<!DOCTYPE html><html> <head> <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <style type="text/css"> body, table, td, a{-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}table{border-collapse: collapse !important;}body{height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important;}@media screen and (max-width: 525px){.wrapper{width: 100% !important; max-width: 100% !important;}.responsive-table{width: 100% !important;}.padding{padding: 10px 5% 15px 5% !important;}.section-padding{padding: 0 15px 50px 15px !important;}}.form-container{margin-bottom: 24px; padding: 20px; border: 1px dashed #ccc;}.form-heading{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 400; text-align: left; line-height: 20px; font-size: 18px; margin: 0 0 8px; padding: 0;}.form-answer{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 300; text-align: left; line-height: 20px; font-size: 16px; margin: 0 0 24px; padding: 0;}div[style*="margin: 16px 0;"]{margin: 0 !important;}</style> </head> <body style="margin: 0 !important; padding: 0 !important; background: #fff"> <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " ></div><table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0" > <tr> <td style=" padding: 0 0 0 0; font-size: 16px; line-height: 25px; color: #232323; " class="padding message-content" > <h2>New Contact Message</h2> <div class="form-container">${data.name}</div></td></tr></table> </td></tr></table> </td></tr></table> </td></tr></table> </body></html>`,
-        subject: data.subject,
-      });
-
-      return res.status(200).json({ success: true });
-    } catch (err: any) {
-      console.log(err);
-      return res.status(400).json({ message: err.message });
+      transporter.sendMail(mailOptions);
+      // Send success
+      return res.status(200).send({ success: "good" });
+    } catch (error: any) {
+      return res.status(400).json({ error: error.message });
     }
   }
-  return res.status(400).json({ message: "Bad request" });
+
+  return res.status(400).json({ error: "Bad request" });
 };
+
 export default handler;
