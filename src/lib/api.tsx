18d6a38806f3e4be2b4e919770d00b8f9eaae80b
VensanDrot <vensandrot@gmail.com>
2023-07-27 02:23:26 -0400

    checking error

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -1,28 +1,11 @@
-export const sendContactForm = async (data: any) => {
+export const sendContactForm = async (data: {
+  name: string;
+  subject: string;
+  email: string;
+  number: string;
+  message: string;
+}) => {
   return await fetch("/api/contact", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
-};
-
-export const getLockedDates = async (date: any) => {
-  return await fetch("/api/getDates", {
-    method: "POST",
-    body: JSON.stringify(date),
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
-};
-
-export const getBookingInfo = async (data: any) => {
-  console.log(data);
-  return await fetch("/api/bookDate", {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
