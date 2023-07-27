91f8cda2654750a5b1c6bf8c3a1d190a42179707
VensanDrot <vensandrot@gmail.com>
2023-07-27 03:59:09 -0400

    error fixed going up

--- src/pages/api/getDates.tsx
+++ src/pages/api/getDates.tsx
@@ -7,7 +7,7 @@ const getDates = async (req: NextApiRequest, res: NextApiResponse) => {
   const bookingDay = req.body;
   try {
     console.log("here");
-    const data = await prisma.booking.findMany();
+
     console.log("here1");
     return res.status(200).json(data);
   } catch (error) {
