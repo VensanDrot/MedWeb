08a185ce686be9b30960584c1d4ac878a5104fe9
VensanDrot <vensandrot@gmail.com>
2023-07-27 02:52:59 -0400

    Commit

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -16,35 +16,4 @@ export const sendContactForm = async (data: {
   });
 };
 
-/* 
-
-export const getLockedDates = async (date: string) => {
-  console.log(date);
-  return await fetch("/api/getDates", {
-    method: "POST",
-    body: JSON.stringify(date),
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
-};
-
-export const getBookingInfo = async (data: {
-  name: string;
-  email: string;
-  number: string;
-  justDate: string;
-  dateTime: string;
-}) => {
-  console.log(data);
-  return await fetch("/api/bookDate", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
-};
-*/
+/* */
